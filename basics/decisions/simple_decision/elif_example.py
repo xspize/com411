18a83f7ebf610288160a7320c0e255e5de036077
repl.it user <replituser@example.com>
2020-10-05 23:03:23 +0000

    example

---
+++ basics/decisions/simple_decision/elif_example.py
@@ -0,0 +1,18 @@
+##Note how the elif is used to check a related condition and provide an alternative and mutually exclusive path.
+
+##Let us consider another example:
+
+print("What are you?")
+
+entity = input()
+
+if (entity == "Human"):
+    print("You are a human!")
+elif (entity == "Robot"):
+    print("You are a robot!")
+elif (entity == "Animal"):
+    print("You are an animal!")
+else:
+    print("I do not know what you are!")
+
+print("Analysis complete.")
