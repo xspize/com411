56ba5efd18f42f501a7736f88cfc7893880420ca
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-06 12:54:10 +0100

    Added code to demonstrate the use of multiple nested statements.

--- basics/decisions/nested_decision/nestception.py
+++ basics/decisions/nested_decision/nestception.py
@@ -1,1 +1,34 @@
+##The program should start by displaying the message "Where should I look?"
+print("Where Should I look?")
+##The program should then read the user's response
+response = input()
 
+if (response == "in the bedroom"):
+  print("Where in the bedroom should I look?")
+  under_bed = input()
+  if (under_bed == "under the bed"):
+    print("Found some shoes but no battery")
+  else:
+    print("Found some mess but no battery.")
+
+elif(response == "in the bathroom"):
+    print("Where in the bathroom should I look?")
+    baththub = input()
+  
+    if (baththub == "in the bathtub"):
+      print("Found a rubber duck but no battery")
+    else:
+      print("Found a wet surface but no battery.")
+
+elif(response == "in the lab"):
+  print("Where in the lab should I look?")
+  table = input()
+
+  if (table == "on the table"):
+      print("Yes! I found my battery!")
+  else:
+    print("Found a wet surface but no battery.")
+
+
+else:
+  print("I do not know where that is but I will keep looking.")
