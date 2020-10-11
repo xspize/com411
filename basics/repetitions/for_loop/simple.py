9d2d266dccafc13539175af777d95c80ccc6c958
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-11 20:16:21 +0100

    Added code to demonstrate the use of a simple for loop.

--- basics/repetitions/for_loop/simple.py
+++ basics/repetitions/for_loop/simple.py
@@ -1,1 +1,19 @@
+#We wish to create a program that allows us to display some mountains.
+#The program should begin by displaying the message "How many mountains should I display?"
+print("How many mountains should I display?")
+mountains = input()
 
+##using function for to show the amount of mountains the user wants
+for count in range (int(mountains)):
+    print("           __")
+    print("          /  \_")
+    print("         /^    \ ")
+    print("        /  ^    \_ ")
+    print("      _/ ^ ^     ^\ ")
+    print("     /  ^     ^    \ ")
+
+
+
+#empty print to give space
+print("")
+print("Done!")
