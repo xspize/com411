0257b4260234f0118ab225d72c58eba2f90682e8
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-11 16:46:10 +0100

    Added code to demonstrate the use of a while loop with ascii art.

--- basics/repetitions/while_loop/ascii.py
+++ basics/repetitions/while_loop/ascii.py
@@ -1,1 +1,15 @@
-
+#We wish to create a program that allows Beep to avoid live cables.
+#The program should start by displaying the message "How many bars should be charged?".
+print("How many bars should be charged?")
+#The program should then read in the user's response.
+user_response = int(input())
+#The program should then create a variable to track the number of bars charged and set this to 0.
+bars_charged = 0
+#The program should then use a while loop to do the following:
+while (bars_charged < user_response):
+  bars_charged = bars_charged +1
+  print("Charging ", end="")
+  print(" | "  * bars_charged)
+else:
+    print("")
+    print("the battery is fully charged.")
