c0c3f7f1c355ff0662b0ea308f7d761d84e1881e
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-11 19:59:19 +0100

    Added code to demonstrate the use of a while loop in calculating the factorial of a number.

--- basics/repetitions/while_loop/factorial.py
+++ basics/repetitions/while_loop/factorial.py
@@ -1,1 +1,15 @@
+#We wish to create a program to help Bop calculate the factorial of a specified number.
+#The program should start by displaying the message "Please enter a number:".
+print("Please enter a number:")
+number = int(input())
 
+#for the calculation
+calculation = 0
+# result
+result = 1
+#loop  to calculate the factorial for the specified number
+while (calculation < number):
+  calculation = calculation + 1
+  result = result * calculation
+#for showing the results
+print ("The factorial is {}".format(result))
