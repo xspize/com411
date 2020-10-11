b3d83863a3894ba87b92101e18cad8fdd082109a
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-11 19:08:30 +0100

    Added code to demonstrate the use of a while loop with len function.

--- basics/repetitions/while_loop/len.py
+++ basics/repetitions/while_loop/len.py
@@ -1,1 +1,8 @@
+#We wish to create a program to help the robot communicate with Beep.
 
+#The program should display the message "Please enter a phrase:"
+print("Please enter a phrase:")
+#The program should then read in the user's response.
+user_input = input()
+#The program should then display the word "Bop" repeatedly x number of times where x is the number of characters in the phrase entered by the user.
+print(" Bop " * len(user_input))
