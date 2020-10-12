318d355db3b3beb749fea030145755d892859e56
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-12 20:09:24 +0100

    Added code to demonstrate the use of membership operators with a for loop.

--- basics/repetitions/for_loop/membership_operators.py
+++ basics/repetitions/for_loop/membership_operators.py
@@ -1,1 +1,15 @@
+#We wish to create a program that allows us to display a phrase in reverse.
+#The program should begin by displaying the message "What phrase do you see?".
+print("What phrase do you see?")
+user_response = input()
+
+print("\t Reversing...")
+
+reversed = ""
+
+for number in user_response:
+  reversed = number + reversed
+
+print(reversed)
+
 
