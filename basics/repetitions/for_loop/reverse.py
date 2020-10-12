aa571bc952cc57f12356c313b58bb2fc637f404c
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-12 20:01:40 +0100

    Added code to demonstrate how to reverse a string using a for loop.

--- basics/repetitions/for_loop/reverse.py
+++ basics/repetitions/for_loop/reverse.py
@@ -1,1 +1,10 @@
+#We wish to create a program that allows us to display a phrase in reverse.
 
+#The program should begin by displaying the message What phrase do you see?
+
+print("What phrase do you see?")
+phrase = str(input())
+
+print("\nReversing...The phrase is ", end="")
+for reverse in range (len(phrase) -1, -1, -1):
+  print(phrase[reverse],end="")
