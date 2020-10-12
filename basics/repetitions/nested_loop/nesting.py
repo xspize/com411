03e16d3c4f0b968a37e244c3c5419806be3ac193
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-12 20:21:36 +0100

    Added code to demonstrate the use of nested loops and decisions.

--- basics/repetitions/nested_loop/nesting.py
+++ basics/repetitions/nested_loop/nesting.py
@@ -1,1 +1,22 @@
+# Beep's program should first ask the user to enter a sequence of characters consisting of dashes and two markers e.g. "X----X".
+print("Please enter a sequence:")
+user_input = input()
 
+print("Please enter the character for the marker:")
+marker = input()
+
+
+marker1 = -1
+marker2 = -1
+
+for position in range(0, len(user_input), 1):
+    letter = user_input[position]
+
+    if (letter == marker):
+        if (marker1 == -1):
+            marker1 = position
+        else:
+            marker2 = position
+
+
+print("The distance between the markers is", marker2 - marker1 - 1)
