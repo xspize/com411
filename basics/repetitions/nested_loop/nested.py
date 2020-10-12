fb178090549d4b8bd877e57359cc327cb8ba7bd9
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-12 20:16:53 +0100

    Added code to demonstrate the use of one loop nested is another.

--- basics/repetitions/nested_loop/nested.py
+++ basics/repetitions/nested_loop/nested.py
@@ -1,1 +1,13 @@
+#We wish to create a program that allows us to display a grid of ASCII emoji.
+#The program should start by asking the user how many rows and columns they would like.
+print("How many rows should I have?")
+rows = int(input())
 
+print("How many columns should I have?")
+columns = int(input())
+
+# Placing the row variable inside the first loop to make it repeat the same amount that the user inputs, then after, inside that loop, the loop will show the amount of columns desired by the user.
+for row in range(0, rows, 1):
+    for column in range(0, columns, 1):
+        print(":-(", end="")
+    print()
