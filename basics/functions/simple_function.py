fd671760293378c3667d55471e8afe522b8fbc71
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-13 11:05:53 +0100

    Added code to demonstrate the use of a user-defined function

--- basics/functions/simple_function.py
+++ basics/functions/simple_function.py
@@ -1,1 +1,11 @@
+#The program should start by defining a function that has the name listen and which takes 0 parameters.
 
+#create function
+def listen():
+  print("What sound did I hear?")
+  user_input = input()
+  print("that was a loud {}!".format(user_input))
+
+
+#run function
+listen()
