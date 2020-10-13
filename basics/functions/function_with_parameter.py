c045f20fbbb7ccf82e3ac5be034267077de8ffd5
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-13 11:33:00 +0100

    Added code to demonstrate the use of a user-defined function with a parameter.

--- basics/functions/function_with_parameter.py
+++ basics/functions/function_with_parameter.py
@@ -1,1 +1,15 @@
+#define function
+def escape_by(plan):
+  if (plan == "jumping over"):
+    print("We cannot escape that way! The boulder is too big!")
+  else:
+      if (plan == "running around"):
+        print("We cannot escape that way! The boulder is moving too fast!")
+      if (plan == "going deeper"):
+        print("That might just work! Let's go deeper!")
 
+
+# run function
+escape_by("jumping over")
+escape_by("running around")
+escape_by("going deeper") 
