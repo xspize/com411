b7c9f50a89ff7a80e62dc3db72916ba5855562c7
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-13 10:47:11 +0100

    Added code to demonstrate the use of built-in functions: len, ord.

--- basics/functions/ascii_code.py
+++ basics/functions/ascii_code.py
@@ -1,1 +1,8 @@
+print("Program Started!")
+print("Please enter a standard character:")
+letter = str(input())
 
+if len(letter) == 1:
+  print("The ASCII code for {} is {}".format(letter,ord(letter)))
+  
+print("program ended!")
