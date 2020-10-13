88cf73c9a9bab04f24f1ba5036fbbddf3939f4c8
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-14 00:40:58 +0100

    Added code to demonstrate the implementation of multiple user defined functions.

--- basics/functions/multiple_functions.py
+++ basics/functions/multiple_functions.py
@@ -1,1 +1,16 @@
+def display_ladder(steps):
 
+  print("|  |")
+  #This function should display an ASCII ladder with the same number of steps as the value of the parameter steps.
+  for step in range(steps):
+    print("****")
+    print("|  |")
+
+#The second function should be named create_ladder and should have no parameters.  This function should ask the user for the number of steps and then use the value in a call to the first function.
+def create_ladder():
+  print("How many steps remain?")
+  steps = int(input())
+  display_ladder(steps)
+
+#Finally, the program should contain a call to the second function
+create_ladder()
