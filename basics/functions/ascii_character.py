d010fb766049873df367d47e66769290b103a6f8
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-13 12:12:48 +0100

    Update ascii_character.py

--- basics/functions/ascii_character.py
+++ basics/functions/ascii_character.py
@@ -3,17 +3,10 @@ print("Please enter an ASCII code:")
 #user input
 user_code = int(input())
 
-#Check that the number is in the range 32 - 126 
-range1 = 32
-range2 = 126
-
-# the number in within the range then the program should
-#determine the ASCII character from the number using the built-in function chr().
-if user_code in range (range1, range2):
-  letter = chr(user_code)
-  #display the message
-  print("The character represented by the ASCII code {} is {}".format(user_code, letter))
+if(user_code >= 32 and user_code <= 126):
+ascii_letter = chr(user_code)
+print("The character represented by the ASCII code {} is {}.".format(user_input, ascii_leter))
 else:
-  print("error")
-  #end message
-print("Program ended.")
+  print("invalid number!")
+  
+ print("Program ended!")
