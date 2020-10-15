07aa3fcc16ddc5b938220abe85c449568f0f34f0
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-15 14:02:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("test")
+import basics.output.simple_message as simple_message
+import basics.output.multiline_message as multiline_message
+
+def run_block_a():
+    print("Which program in 'Block A: Basics' do you wish to run?")
+    response = input()
+    if (response == "simple_message"):
+        simple_message.run()
+    elif (response == "multiline_message"):
+        multiline_message.run()
+
+
+def run():
+    is_running = True
+
+    while(is_running):
+        print("What would you like to do?")
+        print("[a] Run 'Block A: Basics' programs")
+        print("[q] Quit")
+        response = input()
+
+        if (response == "a"):
+            run_block_a()
+        elif (response == "q"):
+            break
+        else:
+            print("Invalid option! Please try again.")
+
+run()
