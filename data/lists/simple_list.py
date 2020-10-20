7c248915e505d1cc2c3218a8b79c8abdb2858f9a
repl.it user <replituser@example.com>
2020-10-20 09:14:03 +0000

    Added code to demonstrate a simple list.

--- data/lists/simple_list.py
+++ data/lists/simple_list.py
@@ -1,1 +1,11 @@
+# The first function should be named directions and should have no parameters.
+def directions():
+  #The function should populate the list with the following items: "Move Forward", "Move Backward", "Turn Left" and "Turn Right". 
+  directions = ["Move forward","Move backward","Turn Left","Turn right"]
+  return directions
+ 
 
+def run():
+  print(directions())
+
+run()
