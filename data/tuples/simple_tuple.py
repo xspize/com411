da7bc33f540bd34470b17da3409dfe3989e4c030
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-26 18:20:31 +0000

    Added code to demonstrate a simple tuple.

--- data/tuples/simple_tuple.py
+++ data/tuples/simple_tuple.py
@@ -1,1 +1,15 @@
-
+#The first function should be named likelihood and should have no parameters.
+def likelihood():
+#create a tuple and populate it with numbers
+    likelihoods = (50, 38, 27, 99, 4)
+#return those numbers
+    return likelihoods
+# declare function to store in a local variable the data from likelihood.
+def run():
+# stores data from another function into a local function
+    local_likelihood = likelihood()
+# prints the minimum number inside the tuple.
+    print("Minimum likelihood of falling: {}%".format(min(local_likelihood)))
+    
+run()
+    
