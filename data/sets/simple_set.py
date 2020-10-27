c44854bed1ee287477ac60ab2a0aa6e99a9c695c
João Soares <71902456+xspize@users.noreply.github.com>
2020-10-27 10:02:30 +0000

    Added code to demonstrate a simple set.

--- data/sets/simple_set.py
+++ data/sets/simple_set.py
@@ -1,1 +1,15 @@
+#Simple Set Program
+def observed():
+# creates a set followed by a list of names 
+    observations = {"Flying Car", "Sky Scraper", "Laser" , "Dome" }
+# returns the set
+    return observations
+    
+def run():
+#stores the value of the set in a local variable
+    return_results = observed()
+#prints the results from the other function that is stored in a local variable
+    print(return_results)
 
+#runs the function
+run()
