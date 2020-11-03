3b5f0b50f5d02f494efd2741443c6754ec844215
João Soares <71902456+xspize@users.noreply.github.com>
2020-11-03 10:47:53 +0000

    Added code to demonstrate how to read a text file.

--- data/files/txt/read.py
+++ data/files/txt/read.py
@@ -1,16 +1,20 @@
 import os
 
+#The first function should be named search and should take 1 parameter that represents a file name
 def search(location):
   print("Searching...")
+#The function should then open the specified file for reading.
   with open("data/files/txt/location.txt") as file:
+#reads in between every single line of the file
     for line in file:
       print(f"Looked in {line}")
 
-    print("...Done!")
+  print("...Done!")
 
 def run():
+#function with file name as a parameter
   search("location.txt")
 
-
+#runs the program
 run()
 
