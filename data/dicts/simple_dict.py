fd4ed5bd94d371018726fceebee89f7fcf866bf2
João Soares <71902456+xspize@users.noreply.github.com>
2020-11-03 09:52:02 +0000

    Added code to demonstrate a simple dictionary.

--- data/dicts/simple_dict.py
+++ data/dicts/simple_dict.py
@@ -1,1 +1,17 @@
-
+#creates function
+def pattern():
+#creates a dictionary populated with words and numbers
+    sequences = {"Short Sequence":3, "Medium Sequence":2, "Long Sequence":1}
+#returns the directionary
+    return sequences
+    
+#runs the program    
+def run():
+#transfer the other function to an inside function
+    local_sequences = pattern()
+#prints the dictionary out
+    print(local_sequences)
+    
+#runs the program
+run()
+    
