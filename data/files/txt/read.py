663f28ae5226771371563b696bcefdfab3b7370a
João Soares <71902456+xspize@users.noreply.github.com>
2020-11-03 10:43:27 +0000

    Update read.py

--- data/files/txt/read.py
+++ data/files/txt/read.py
@@ -1,1 +1,16 @@
+import os
+
+def search(location):
+  print("Searching...")
+  with open("data/files/txt/location.txt") as file:
+    for line in file:
+      print(f"Looked in {line}")
+
+    print("...Done!")
+
+def run():
+  search("location.txt")
+
+
+run()
 
