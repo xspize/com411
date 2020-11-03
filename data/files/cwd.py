5e7c2e39cfe27e31aa08ae1ba22a5566d3acbdc3
João Soares <71902456+xspize@users.noreply.github.com>
2020-11-03 10:20:32 +0000

    Added code to display cwd information.

--- data/files/cwd.py
+++ data/files/cwd.py
@@ -1,1 +1,19 @@
+#import module os 
+import os
 
+def cwd():
+#os.getcwd displays the file path of the current working folder
+  path = os.getcwd()
+  print(f"The current working folder is: {path}")
+  print("The folder contains the following:")
+#to display the content of this folder using the method listdir of the module
+  for file in os.listdir(path):
+    print(file)
+
+#function to run the program
+def run():
+  print("Processing...")
+  cwd()
+
+#runs the program
+run()
