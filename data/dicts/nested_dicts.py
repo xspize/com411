2fa739ca4f1a88d37350d8c741b7d0564cd7cb93
João Soares <71902456+xspize@users.noreply.github.com>
2020-11-06 21:25:50 +0000

    Update nested_dicts.py

--- data/dicts/nested_dicts.py
+++ data/dicts/nested_dicts.py
@@ -23,3 +23,39 @@ def run():
         print(f"{key}: {value}")
 
 run()
+
+#other way
+
+def short_pattern():
+  pattern = {}
+  pattern["sequence"] = 101
+  pattern["ocurrences"] = 5
+
+  return pattern
+
+def medium_pattern():
+  pattern = {}
+  pattern["sequence"] = 111000
+  pattern["ocurrences"] = 25
+
+  return pattern
+
+def long_pattern():
+  pattern = {}
+  pattern["sequence"] = 1100110011001100
+  pattern["ocurrences"] = 50
+
+  return pattern
+
+def run():
+  print("Analysing patterns...") 
+  value_pairs = {
+  "Short sequence":short_pattern(),
+  "Medium sequence":medium_pattern(),
+  "Long sequence":long_pattern(),
+  } 
+
+  for key,value in value_pairs.items():
+    print(f"{key}:{value}")
+
+run()
