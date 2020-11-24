500c00a8413c04b6518fc39f8dcf11b7a2d1d0ea
João Soares <71902456+xspize@users.noreply.github.com>
2020-11-24 10:09:43 +0000

    Update temps.py

--- visual/subplots/temps.py
+++ visual/subplots/temps.py
@@ -15,9 +15,12 @@ def read_data(file_path):
 def run():
   #The function should start by calling the first function with the file path and name visual/subplots/temps.txt and assign the resulting list of values to a local variable named data.
   data = read_data('visual/subplots/temps.txt')
-  #The function should start by calling the first function with the file path and name visual/subplots/temps.txt and assign the resulting list of values to a local variable named data
+  #The function should then display the list of values named data in a Matplotlib graph consisting 
+  #of two subplots placed horizontally.  The first subplot should show the data as a line graph and the second subplot should show the data as a bar chart.
   fig, (ax1, ax2) = plt.subplots(1, 2)
+  #line graph
   ax1.plot(range(len(data)), data)
+  #bar graph
   ax2.bar(range(len(data)), data)
   plt.show()
 
