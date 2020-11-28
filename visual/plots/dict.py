e15fad7af0cd8778d01c38bfb6b46e1cdbc03c1e
João Soares <71902456+xspize@users.noreply.github.com>
2020-11-28 21:57:30 +0000

    Added code to display line plots using a dictionary.

--- visual/plots/dict.py
+++ visual/plots/dict.py
@@ -2,43 +2,43 @@ import matplotlib.pyplot as plt
 import random as rnd
 
 def data():
-  paths = {}
-
-  print("What kind of line would you like? :, -- or -?")
-  line = input()
-  print("What colour would you like? r, g or b?")
-  colour = input()
-  print("What style of marker would you like? o,s or ^")
-  marker = input()
-
-#The function should then store the line style, colour, marker style in the dictionary as key-value pairs
-  paths['line'] = line
-  paths['colour'] = colour
-  paths['marker'] = marker
-
-#return the dictionary
-  return paths
+    paths = {}
+    print("What type of line would you like? :, -- or -")
+    line = input()
+    print("What colour? r, g or b?")
+    colour = input()
+    print("What style of marker would you like o,s or ^ ")
+    marker = input()
+    
+    paths['line'] = line
+    paths['colour'] = colour
+    paths['marker'] = marker
+    
+    return paths
 
 def generate():
-  print("How many lines would you like to display?")
-  lines_display = int(input())
-
-
-  for number in range(lines_display):
-    values = data()
-
-    x = rnd.sample(range(1,10),5)
-    y = rnd.sample(range(1,10),5)
-
-    format = f"{values['colour']}{values['line']}{values['marker']}"
-    plt.plot(x, y, format)
-
-  plt.show()
-
+    print("How many lines would you like to display?")
+    
+    lines = int(input())
+    
+    #The function should then create a loop that iterates as many times as the number of lines specified by user
+    for number in range (lines):
+        
+        #Call the first function and store the result in a variable named values.
+        values = data()
+        
+        #Display a line graph using the values stored in values and random values for x and y (see random.sample)
+        x = rnd.sample(range(1,30),5)
+        y = rnd.sample(range(1,30),5)
+        
+        format = f"{values['colour']}{values['line']}{values['marker']}"
+        
+        plt.plot(x,y, format)
+        plt.show()
+        
 def run():
-  print("Running...")
-  generate()
-  print("Done!")   
-
-
+    print("Running...")
+    generate()
+    print("Done!")
+    
 run()
