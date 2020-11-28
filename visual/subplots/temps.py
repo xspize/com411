6f86fffd5957fc5b3a3e7f9c318d71271bd4d498
João Soares <71902456+xspize@users.noreply.github.com>
2020-11-28 22:38:51 +0000

    Added code to display simple subplots

--- visual/subplots/temps.py
+++ visual/subplots/temps.py
@@ -1,27 +1,31 @@
 import matplotlib.pyplot as plt
 
+
 def read_data(file_path):
-  #creates empty list
-  temps = []
-  #opens file 
-  with open(file_path) as file:
-    #loops each line in file
-    for line in file:
-      #The function should then read each line in the file and store it into a list (remember to strip and int() or float()).
-      temps.append(float(line.strip()))
-      #the function should return the list of values.
-  return temps
+    #creates empty list
+    values = []
+    #opens file temps.txt
+    with open('temps.txt') as file:
+        #loops lines inside file
+        for line in file:
+            #adds values to empty list
+            values.append(float(line.strip()))
+    return values
 
 def run():
-  #The function should start by calling the first function with the file path and name visual/subplots/temps.txt and assign the resulting list of values to a local variable named data.
-  data = read_data('visual/subplots/temps.txt')
-  #The function should then display the list of values named data in a Matplotlib graph consisting 
-  #of two subplots placed horizontally.  The first subplot should show the data as a line graph and the second subplot should show the data as a bar chart.
-  fig, (ax1, ax2) = plt.subplots(1, 2)
-  #line graph
-  ax1.plot(range(len(data)), data)
-  #bar graph
-  ax2.bar(range(len(data)), data)
-  plt.show()
-
+    # inserts data from function inside local function
+    data = read_data('temps.txt')
+    
+    #create subplots, fig is figure, ax1 and ax2 are both graphics, subplots(x,y) y creates 2 columns
+    fig, (ax1,ax2) = plt.subplots(1, 2)
+    
+    #changes ax1 that is a plot
+    ax1.plot(range(len(data)),data)
+    
+    #changes ax1 that is a bar
+    ax2.bar(range(len(data)),data)
+    
+    plt.show()
+    
+#runs programn   
 run()
