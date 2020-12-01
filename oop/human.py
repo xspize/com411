a06c6ead99b639cb2db359d2cd3c64f55bd7403c
João Soares <71902456+xspize@users.noreply.github.com>
2020-12-01 11:28:03 +0000

    Updated classes human and robot with instance methods.

--- oop/human.py
+++ oop/human.py
@@ -3,19 +3,29 @@ class Robot:
     
     #A class atribute
     laws = "Protect, Obey and Survive"
+    MAX_ENERGY = 100
     
     # A class method
     def the_laws(self):
         print(Robot.laws)
+
+    def grow(self):
+        self.age +=1
+    
         
     #an initialiser (special instance method)
     def __init__(self):
         
         #An Instance attribute
         self.name = "Robot"
-        self.age = 0
+        self.age = 100
+        self.eat = 30
+        self.move = 40
+        self.energy = Robot.MAX_ENERGY
+        self.eat = 10 + Robot.MAX_ENERGY
+        self.move = Robot.MAX_ENERGY - 40
         
-    #repr returns a formal string representation of the object    
+    #repr returns a formal string representation of the object  
     def __repr__(self):
         return f'robot(name={self.name}, age={self.age})'
         
@@ -39,28 +49,36 @@ class Human:
     MAX_ENERGY = 100
     
     #initialiser
-    def __init__(self):
+    def __init__(self, name, age):
         
         #instance attribute
-        self.name = "Human"
-        self.age =  0
+        self.name = name
+        self.age = age
         self.energy = Human.MAX_ENERGY
+        self.eat = 10 + Human.MAX_ENERGY
+        self.move = Human.MAX_ENERGY - 40
+        
+    def grow(self):
+        self.age  +=1
      
-    #str returns an informal string representation of the object    
+    #str returns an informal string representation of the object   
+
     def __str__(self):
         return f'My name is {self.name} and I am {self.age} years old.'
         
     #instance method
     def display(self):
-        print(f"I am  {self.name} and have {self.energy} energy.")
+        print(f"I am  {self.name} and I ate a burger which increased my energy to {self.eat} and then I started running and wasted {self.move} energy.")
         
         
 # object
 if (__name__ == "__main__"):
-  human = Human()
-  human.display()
+  Joao = Human("João", 23)
+  Joao.display()
+  Joao.grow()
+
   #necessary to show the str or repr
-  print(human)
+  print(Joao)
   
         
     
