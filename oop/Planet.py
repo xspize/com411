b762d4704bca174456e5149300495c93e0bc5379
João Soares <71902456+xspize@users.noreply.github.com>
2020-12-20 11:45:17 +0000

    Updated classes human and robot with instance methods.

--- oop/Planet.py
+++ oop/Planet.py
@@ -9,43 +9,34 @@ class Planet:
 
 #Replace the list of humans and the list of robots with a single dictionary named inhabitants which is an instance attribute.  Initially, the dictionary should looks as follows
     def __init__(self):
-        self.inhabitants = {
-        'humans' : [],
-        'robots' : []
-        }
+        self.inhabitants = []
 
     # magic methods
 
 # __repr__ - returns a string containing a formal representation of a planet object (includes the list of humans and the list of robots)
     def __repr__(self):
-        return f"planet(humans={self.inhabitants['humans']}, robots={self.inhabitants['robots']})"
+        return f"planet(inhabitants={self.inhabitants})"
 
 #   __str__ - return a string containing an informal representation of a planet object (includes the list of humans and the list of robots)
 
     def __str__(self):
-        return f"This planet has {len(self.inhabitants['humans'])} humans and {len(self.inhabitants['robots'])} robots."
+        return f"This planet has {len(self.inhabitants)} inhabitants."
 
     # instance methods
 
-    def add_human(self,human):
-        self.inhabitants['humans'].append(human)
 
-    def add_robot(self,robot):
-        self.inhabitants['robots'].append(robot)
+    def add(self, inhabitant):
+        self.inhabitants.append(inhabitant)
 
-    def remove_human(self, human):
-        self.inhabitants['humans'].remove(human)
 
-    def remove_robot(self, robot):
-        self.inhabitants['robots'].remove(robot)
+    def remove(self, inhabitant):
+        self.inhabitants.remove(inhabitant)
 
 if (__name__ == "__main__"):
   planet = Planet()
   print(repr(planet))
   prins = Human("Prins")
-  planet.add_human(prins)
-  bop = Robot("Bop")
-  planet.add_robot(bop)
+  planet.add(prins)
   print(repr(planet))
   print(planet)
 
